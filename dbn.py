e12527583b0f704b2c0f88c7fba968fd31427d58
JonasWechsler <jonaswechsler@gmail.com>
2018-03-24 19:48:35 +0100

    Refactor

--- dbn.py
+++ dbn.py
@@ -1,24 +1,7 @@
 import numpy as np
 import csv
 from sklearn.neural_network import BernoulliRBM
-
-def error(a, b):
-    return (a != b).sum()
-
-def percent_error(a, b):
-    return sum(error(*x) for x in zip(a, b))/float(len(a)*len(a[0]))
-
-train_data = []
-with open('binMNIST_data\\bindigit_trn.csv') as f:
-	reader = csv.reader(f)
-	for row in reader:
-		train_data.append(np.array([int(_) for _ in row]))
-
-test_data = []
-with open('binMNIST_data\\bindigit_tst.csv') as f:
-	reader = csv.reader(f)
-	for row in reader:
-		test_data.append(np.array([int(_) for _ in row]))
+from rbm import error, percent_error
 
 def make_rbm(data, components=50, n_iter=10):
     model = BernoulliRBM(n_components=components, learning_rate=0.1, batch_size=10, n_iter=n_iter, 
@@ -46,13 +29,20 @@ def run_together(data, layers):
     return data
 
 if __name__ == "__main__":
-    dbn = make_dbn(train_data, [512, 50], [1, 500])
+    train_data = []
+    test_data = []
     
-    #first = make_rbm(train_data, 150)
-    #first_out = first._sample_hiddens(train_data, first.random_state_)
-    ##first_out = np.round(first._mean_hiddens(train_data)).astype(int)
-    #
-    #second = make_rbm(first_out, 50)
+    with open('binMNIST_data\\bindigit_trn.csv') as f:
+    	reader = csv.reader(f)
+    	for row in reader:
+    		train_data.append(np.array([int(_) for _ in row]))
+    
+    with open('binMNIST_data\\bindigit_tst.csv') as f:
+    	reader = csv.reader(f)
+    	for row in reader:
+    		test_data.append(np.array([int(_) for _ in row]))
+    
+    dbn = make_dbn(train_data, [512, 50], [1, 500])
     
     print("sample / sample error: {}".format(percent_error(dbn[0]._sample_hiddens(train_data, dbn[0].random_state_), dbn[0]._sample_hiddens(train_data, dbn[0].random_state_))))
     print("rounded mean / sample error: {}".format(percent_error(np.round(dbn[0]._mean_hiddens(train_data)).astype(int), dbn[0]._sample_hiddens(train_data, dbn[0].random_state_))))
